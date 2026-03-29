462ca44bbaaf8c51397780f56791967d883694fe
Andy <59784196+RightCliick@users.noreply.github.com>
2026-03-29 01:46:25 -0400

    Add files via upload

---
+++ OOTPROTOTYPE1.lua
@@ -0,0 +1,9 @@
+--[[ 
+This script is meant to be used using the Lua Console in the BizHawk emulator
+Using the GLideN64 Plugin, the aspect ratio was set to 16:9,
+and the video resolution is set to 1920 x 1080.
+]]--
+
+while true do
+	emu.frameadvance();
+end
