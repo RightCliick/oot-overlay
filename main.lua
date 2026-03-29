3a2c7211b0d232282abfc9d60ef6cc041d62eba3
Andy O <oandy434@gmail.com>
2026-03-29 15:47:35 -0400

    just adding some stuff

---
+++ main.lua
@@ -0,0 +1,28 @@
+--[[ 
+This script is meant to be used using the Lua Console in the BizHawk emulator
+Using the GLideN64 Plugin, the aspect ratio was set to 16:9,
+and the video resolution is set to 1920 x 1080.
+]]--
+
+-- NOTE* I am putting everything into this singular lua file for this prototype, further iterations will be split up into more files
+
+-- state variables
+showObjective = false
+showHints = false
+	
+-- draw overlay functions, toggleable with controller input
+	if showObjective then
+
+	if showHints then
+
+
+-- read memory function implementation
+
+
+-- constant game loop
+while true do
+
+
+
+	emu.frameadvance();
+end
